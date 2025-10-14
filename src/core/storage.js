8c896ff64dd1d44a5ca839cf847d77c3ee9d759b
Creepmond <creepmondsamson@gmail.com>
2025-10-14 17:10:32 +0800

    Completed Saves functionality

--- src/core/storage.js
+++ src/core/storage.js
@@ -1,23 +1,42 @@
-import { isDecimal, isSet } from "@/utility/typecheck";
+import { isSet } from "@/utility/typecheck";
 
-function classPointer(value) {
-   console.log(value)
+function classPointer(_, value) {
    if ( isSet(value) ) return { _type: 'Set', _value: [...value] };
-   if ( isDecimal(value) ) return { _type: 'Decimal', _value: value };
 
    return value;
 }
 
 function classReader(_, data) {
-   if (data._type === 'Set') return new Set(data._value);
-   if (data._type === 'Decimal') return new Decimal(data._value);
+   if (data && data._type === 'Set') return new Set(data._value);
+   if (typeof data !== 'string') return data;
 
-   return data;
+   // Weird data, because it's either a Decimal, or an actual String... I have no clue if this is the best
+   // way of doing this. Neither do I have any clue how many times I've been self-deprecating for these
+   // kinds of solutions either. Darn me scooby-doo
+
+   let weirdData = undefined;
+   
+   try {
+      weirdData = new Decimal(data);
+   } catch {
+      weirdData = data;
+   } finally {
+      if (isNaN(weirdData.mantissa)) return data;
+      return weirdData;
+   };
 };
+
+
 
 const GameStorage = {
    save() {
       localStorage.setItem("webSave", JSON.stringify(player, classPointer));
+      console.log( 
+         JSON.parse(
+            localStorage.getItem("webSave"),
+            classReader
+         )
+      );
    },
    load() {
       player = JSON.parse(
